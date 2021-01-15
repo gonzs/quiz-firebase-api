94128af231bf67b95163d749242bf7a84371e875
gonzs <gonzalosisnero@gmail.com>
2021-01-15 00:10:36 -0300

    fix token validation

--- functions/src/auth-middleware/index.ts
+++ functions/src/auth-middleware/index.ts
@@ -1,21 +1,22 @@
 import { Request, Response } from 'express';
 import * as admin from 'firebase-admin';
 
-export function authMiddleware(req: Request, res: Response, next: any) {
+export function authMiddleware(req: Request, res: Response, next: any): any {
   const headerToken = req.headers.authorization;
 
   if (!headerToken) return res.status(401).send('No token provided');
 
-  if (headerToken && headerToken.split('')[0] !== 'Bearer')
+  if (headerToken && headerToken.split(' ')[0] !== 'Bearer')
     return res.status(401).send('Invalid token');
 
-  const token = headerToken.split('')[1];
+  const token = headerToken.split(' ')[1];
   const auth = admin.auth();
 
   auth
     .verifyIdToken(token)
-    .then(() => next())
+    .then(() => {
+      // const uid = decodedToken.uid;
+      next();
+    })
     .catch(() => res.status(403).send('Could not authorize'));
-
-  return res.status(200).send('Valid token');
 }
